28d3b20ca44e4fc312fe41e0f151048203a2ef9c
Ibrahim Arif <ibrahimrana232@gmail.com>
2024-03-11 17:47:41 +0500

    clean up

--- .history/src/pages/ScoreView/ScoreView_20240310201634.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-// import './ScoreView.css';
-import classnames from "classnames";
-
-const ScoreView = ({ handleResetClick, score, questions }) => {
-  return <div></div>;
-};
-
-export default ScoreView;
--- .history/src/pages/ScoreView/ScoreView_20240310201721.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-// import './ScoreView.css';
-import classnames from "classnames";
-
-const ScoreView = ({ handleResetClick, score, questions }) => {
-  return <div className="row"></div>;
-};
-
-export default ScoreView;
