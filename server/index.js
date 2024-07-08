65baabe42b8f9e33f7a37bcbcdb24c2d3b8d4404
Kronium345 <awolowodaniel@yahoo.ie>
2024-07-08 14:17:16 +0100

    Fixing CORS issue again

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import morgan from "morgan";
 import mongoose from "mongoose"; // Add mongoose import
 import dotenv from "dotenv"; // Add dotenv import
@@ -26,7 +27,12 @@ const app = express();
 
 // Middlewares
 app.use(express.json());
-
+app.use(
+  cors({
+    origin: ["https://pharmaque.com"],
+    credentials: true,
+  })
+);
 app.use(morgan("tiny"));
 app.use(cookieParser());
 
